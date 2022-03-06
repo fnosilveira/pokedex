367dc7fb773e02d45c5a6346dd3f2ce87a68eb59
Filipe Nogueira Silveira <fnosilveira@gmail.com>
2022-03-06 08:37:48 -0300

    promises add

--- src/App.js
+++ src/App.js
@@ -1,18 +1,36 @@
-import React from 'react';
-
+import React ,{useEffect, useState} from 'react';
+import { getPokemonData, getPokemons } from "./api"
 import './App.css';
 import Navbar from './components/Navbar';
 import Searchbar from './components/Searchbar';
 import Pokedex from './components/Pokedex';
-
-
-
+import { getPokemonData } from './api';
 function App() {
+  const [loading, setLoading] = useState(false)
+  const [pokemons,setPokemons] = useState([])
+  const fetchPokemons = async() => {
+    try{
+      setLoading(true)
+      const data  = await getPokemons();
+      const promises = data.results.map(async (pokemon)=> {
+        return await getPokemonData(pokemon.url)
+      })
+      setPokemons(result);
+      setLoading(false);
+    } catch(error) {
+      console.log("fetchPokemons error", error)
+    }
+  }
+  useEffect(() => {
+   console.log("carregou")
+   fetchPokemons();
+  }, [])
+  
   return (
     <div>
       <Navbar /> 
       <Searchbar />
-      <Pokedex />
+      <Pokedex  pokemons={pokemons.results} loading={loading}/>
 
     </div>
   );
--- src/components/Pokedex.js
+++ src/components/Pokedex.js
@@ -1,14 +1,27 @@
 import React from "react";
+import Pokemon from "./Pokemon";
 
-const Pokedex = () => {
-    return(
-        <div>
-            <div className="pokedex-header">
-                <div><h1>Pokedex</h1></div>
-                <div>Paginação</div>
-            </div>
+const Pokedex = (props) => {
+  const { pokemons, loading } = props;
+  return (
+    <div>
+      <div className="pokedex-header">
+          <h1>Pokedex</h1>
+        <div>Paginação</div>
+      </div>
+      {loading ? (
+        <div>Carregando, aguenta ai...</div>
+      ) : (
+        <div className="pokedex-grid">
+          {pokemons && pokemons.map((pokemon, index) => {
+            return (
+              <Pokemon key={index} pokemon={pokemon} />
+            );
+          })}
         </div>
-    )
-}
+      )}
+    </div>
+  );
+};
 
 export default Pokedex;
